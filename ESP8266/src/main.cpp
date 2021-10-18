edc925e7a635057dafd2109793b3d7c31655c615
Bernhard Schlegel <b.schlegel@monmedi.de>
2021-10-18 08:41:46 +0200

    read temp once during startup (0,9.3)

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -39,7 +39,7 @@ float celsius, fahrenheit;
 String chipid = "";
 uint32_t main_interval_ms = 1000; // 1s default intervall for first iteration
 uint8_t global_relais_state = 0;
-String global_version = "0.9.2";
+String global_version = "0.9.3";
 
 void writeStringToEEPROM(int addrOffset, const String &strToWrite)
 {
@@ -155,6 +155,120 @@ void short_flash_500ms(uint8_t count)
   }
 }
 
+void readTemperature()
+{
+  // put your main code here, to run repeatedly:
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+
+  if (!ds.search(addr))
+  {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+
+  Serial.print("ROM =");
+  for (i = 0; i < 8; i++)
+  {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7])
+  {
+    Serial.println("CRC is not valid!");
+    return;
+  }
+  Serial.println();
+
+  // the first ROM byte indicates which chip
+  switch (addr[0])
+  {
+  case 0x10:
+    Serial.println("  Chip = DS18S20"); // or old DS1820
+    type_s = 1;
+    break;
+  case 0x28:
+    Serial.println("  Chip = DS18B20");
+    type_s = 0;
+    break;
+  case 0x22:
+    Serial.println("  Chip = DS1822");
+    type_s = 0;
+    break;
+  default:
+    Serial.println("Device is not a DS18x20 family device.");
+    return;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1); // start conversion, with parasite power on at the end
+
+  delay(1000); // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE); // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for (i = 0; i < 9; i++)
+  { // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s)
+  {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10)
+    {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  }
+  else
+  {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00)
+      raw = raw & ~7; // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20)
+      raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40)
+      raw = raw & ~1; // 11 bit res, 375 ms
+                      //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.println(" Celsius.");
+  //Serial.print(fahrenheit);
+  //Serial.println(" Fahrenheit");
+
+  String apikey_restored = readStringFromEEPROM(EEPROM_ADDRESS_APIKEY);
+  Serial.println("PARAM apikey from EEPROM = " + apikey_restored);
+}
+
 void setup()
 {
   Serial.println("BierBot Brick 101 starting");
@@ -186,6 +300,9 @@ void setup()
   Serial.begin(115200);
 
   Serial.println("\n Starting");
+
+  // read temperature once
+  readTemperature();
 
   // guid, len 36 + terminator
   // e.g. 550e8400-e29b-11d4-a716-446655440000
@@ -331,120 +448,6 @@ void checkRequest()
     client.stop();
     Serial.println("Client disconnected.");
   }
-}
-
-void readTemperature()
-{
-  // put your main code here, to run repeatedly:
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-
-  if (!ds.search(addr))
-  {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(250);
-    return;
-  }
-
-  Serial.print("ROM =");
-  for (i = 0; i < 8; i++)
-  {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7])
-  {
-    Serial.println("CRC is not valid!");
-    return;
-  }
-  Serial.println();
-
-  // the first ROM byte indicates which chip
-  switch (addr[0])
-  {
-  case 0x10:
-    Serial.println("  Chip = DS18S20"); // or old DS1820
-    type_s = 1;
-    break;
-  case 0x28:
-    Serial.println("  Chip = DS18B20");
-    type_s = 0;
-    break;
-  case 0x22:
-    Serial.println("  Chip = DS1822");
-    type_s = 0;
-    break;
-  default:
-    Serial.println("Device is not a DS18x20 family device.");
-    return;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1); // start conversion, with parasite power on at the end
-
-  delay(1000); // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE); // Read Scratchpad
-
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for (i = 0; i < 9; i++)
-  { // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s)
-  {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10)
-    {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  }
-  else
-  {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00)
-      raw = raw & ~7; // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20)
-      raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40)
-      raw = raw & ~1; // 11 bit res, 375 ms
-                      //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  fahrenheit = celsius * 1.8 + 32.0;
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.println(" Celsius.");
-  //Serial.print(fahrenheit);
-  //Serial.println(" Fahrenheit");
-
-  String apikey_restored = readStringFromEEPROM(EEPROM_ADDRESS_APIKEY);
-  Serial.println("PARAM apikey from EEPROM = " + apikey_restored);
 }
 
 void contactBackend()
